85c2f9381d003611458e09cf53f3b08f6dbdd4e5
Rob Bishop <rob.bishop@raspberrypi.org>
2013-04-10 17:21:33 +0100

    Added cursor support program for testing

--- host_applications/linux/apps/hello_pi/hello_dispmanx/dispmanx.c
+++ host_applications/linux/apps/hello_pi/hello_dispmanx/dispmanx.c
@@ -1,30 +1,3 @@
-/*
-Copyright (c) 2012, Broadcom Europe Ltd
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the copyright holder nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 // A simple demo using dispmanx to display an overlay
 
 #include <stdio.h>
@@ -34,128 +7,102 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unistd.h>
 #include <sys/time.h>
 
-#include "bcm_host.h"
+#include <X11/Xlib.h>
 
-#define WIDTH   200
-#define HEIGHT  200
+#include "bcm_host.h"
 
 #define ALIGN_UP(x,y)  ((x + (y)-1) & ~((y)-1))
 
-typedef struct
-{
-    DISPMANX_DISPLAY_HANDLE_T   display;
-    DISPMANX_MODEINFO_T         info;
-    void                       *image;
-    DISPMANX_UPDATE_HANDLE_T    update;
-    DISPMANX_RESOURCE_HANDLE_T  resource;
-    DISPMANX_ELEMENT_HANDLE_T   element;
-    uint32_t                    vc_image_ptr;
-
-} RECT_VARS_T;
-
-static RECT_VARS_T  gRectVars;
-
 static void FillRect( VC_IMAGE_TYPE_T type, void *image, int pitch, int aligned_height, int x, int y, int w, int h, int val )
 {
-    int         row;
-    int         col;
+  int         row;
+  int         col;
 
-    uint16_t *line = (uint16_t *)image + y * (pitch>>1) + x;
+  uint16_t *line = (uint16_t *)image + y * (pitch>>1) + x;
 
-    for ( row = 0; row < h; row++ )
+  for ( row = 0; row < h; row++ )
     {
-        for ( col = 0; col < w; col++ )
+      for ( col = 0; col < w; col++ )
         {
-            line[col] = val;
+	  line[col] = val;
         }
-        line += (pitch>>1);
+      line += (pitch>>1);
     }
 }
 
 int main(void)
 {
-    RECT_VARS_T    *vars;
-    uint32_t        screen = 0;
-    int             ret;
-    VC_RECT_T       src_rect;
-    VC_RECT_T       dst_rect;
-    VC_IMAGE_TYPE_T type = VC_IMAGE_RGB565;
-    int width = WIDTH, height = HEIGHT;
-    int pitch = ALIGN_UP(width*2, 32);
-    int aligned_height = ALIGN_UP(height, 16);
-    VC_DISPMANX_ALPHA_T alpha = { DISPMANX_FLAGS_ALPHA_FROM_SOURCE | DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS, 
-                             120, /*alpha 0->255*/
-                             0 };
+  uint32_t                    vc_image_ptr;
+  VC_RECT_T                   src_rect;
+  VC_RECT_T                   dst_rect;
+  VC_IMAGE_TYPE_T             type =      VC_IMAGE_RGB565;
+  DISPMANX_DISPLAY_HANDLE_T   dispmanx_display;
+  DISPMANX_RESOURCE_HANDLE_T  resource;
+  DISPMANX_MODEINFO_T         info;
+  DISPMANX_UPDATE_HANDLE_T    update;
+  DISPMANX_ELEMENT_HANDLE_T   element;
+  void                       *image;
+  int                         width = 10; 
+  int                         height = 10;
+  int                         pitch = ALIGN_UP(width*2, 32);
+  int                         aligned_height = ALIGN_UP(height, 16);
+  int                         ret;
 
-    vars = &gRectVars;
+  Display                    *x_display;
+  Window                      root, child;
+  int                         cursor_X, cursor_Y, winX, winY;
+  unsigned int                mask;
 
-    bcm_host_init();
+  bcm_host_init();
 
-    printf("Open display[%i]...\n", screen );
-    vars->display = vc_dispmanx_display_open( screen );
+  dispmanx_display = vc_dispmanx_display_open(0);
+ 
+  ret = vc_dispmanx_display_get_info(dispmanx_display, &info);
+   
+  image = calloc(1, pitch * height);
+  
+  FillRect(type, image, pitch, aligned_height, 0, 0, width, height, 0x0000);
+  
+  resource = vc_dispmanx_resource_create(type, width, height, &vc_image_ptr);
+  
+  vc_dispmanx_rect_set(&dst_rect, 0, 0, width, height);
 
-    ret = vc_dispmanx_display_get_info( vars->display, &vars->info);
-    assert(ret == 0);
-    printf( "Display is %d x %d\n", vars->info.width, vars->info.height );
+  vc_dispmanx_resource_write_data(resource, type, pitch, image, &dst_rect);
 
-    vars->image = calloc( 1, pitch * height );
-    assert(vars->image);
+  update = vc_dispmanx_update_start(0);
 
-    FillRect( type, vars->image, pitch, aligned_height,  0,  0, width,      height,      0xFFFF );
-    FillRect( type, vars->image, pitch, aligned_height,  0,  0, width,      height,      0xF800 );
-    FillRect( type, vars->image, pitch, aligned_height, 20, 20, width - 40, height - 40, 0x07E0 );
-    FillRect( type, vars->image, pitch, aligned_height, 40, 40, width - 80, height - 80, 0x001F );
-
-    vars->resource = vc_dispmanx_resource_create( type,
-                                                  width,
-                                                  height,
-                                                  &vars->vc_image_ptr );
-    assert( vars->resource );
-    vc_dispmanx_rect_set( &dst_rect, 0, 0, width, height);
-    ret = vc_dispmanx_resource_write_data(  vars->resource,
-                                            type,
-                                            pitch,
-                                            vars->image,
-                                            &dst_rect );
-    assert( ret == 0 );
-    vars->update = vc_dispmanx_update_start( 10 );
-    assert( vars->update );
-
-    vc_dispmanx_rect_set( &src_rect, 0, 0, width << 16, height << 16 );
-
-    vc_dispmanx_rect_set( &dst_rect, ( vars->info.width - width ) / 2,
-                                     ( vars->info.height - height ) / 2,
-                                     width,
-                                     height );
-
-    vars->element = vc_dispmanx_element_add(    vars->update,
-                                                vars->display,
+  vc_dispmanx_rect_set(&src_rect, 0, 0, width << 16, height << 16);
+  
+  vc_dispmanx_rect_set(&dst_rect, (info.width - width)/2, (info.height - height)/2, width, height);
+    
+  element = vc_dispmanx_element_add(          update,
+                                                dispmanx_display,
                                                 2000,               // layer
                                                 &dst_rect,
-                                                vars->resource,
+                                                resource,
                                                 &src_rect,
                                                 DISPMANX_PROTECTION_NONE,
-                                                &alpha,
+                                                NULL,
                                                 NULL,             // clamp
                                                 VC_IMAGE_ROT0 );
 
-    ret = vc_dispmanx_update_submit_sync( vars->update );
-    assert( ret == 0 );
+  vc_dispmanx_update_submit_sync(update);
 
-    printf( "Sleeping for 10 seconds...\n" );
-    sleep( 10 );
+  x_display = XOpenDisplay(NULL);
 
-    vars->update = vc_dispmanx_update_start( 10 );
-    assert( vars->update );
-    ret = vc_dispmanx_element_remove( vars->update, vars->element );
-    assert( ret == 0 );
-    ret = vc_dispmanx_update_submit_sync( vars->update );
-    assert( ret == 0 );
-    ret = vc_dispmanx_resource_delete( vars->resource );
-    assert( ret == 0 );
-    ret = vc_dispmanx_display_close( vars->display );
-    assert( ret == 0 );
+  while (1){ //some cursor movement trigger
 
-    return 0;
+    XQueryPointer(x_display,DefaultRootWindow(x_display),&root,&child,&cursor_X,&cursor_Y,&winX,&winY,&mask);    
+    printf("x=%d - y=%d\n", cursor_X, cursor_Y);
+
+    update = vc_dispmanx_update_start(0);
+
+    vc_dispmanx_rect_set(&dst_rect, cursor_X, cursor_Y, width, height);
+
+    vc_dispmanx_element_change_attributes(update, element, 0x4, 2000, NULL, &dst_rect, &src_rect, NULL, VC_IMAGE_ROT0);
+
+    vc_dispmanx_update_submit_sync(update);
+
+  }
+  return 0;
 }
-
